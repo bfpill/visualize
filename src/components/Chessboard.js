d42527639298688a8f62563baf14ba42497a6b45
Max Adam <max@v3rv.com>
2023-03-28 22:11:51 +1100

    Add Piece Working and adding Queens

--- src/components/Chessboard.js
+++ src/components/Chessboard.js
@@ -13,8 +13,8 @@ function Chessboard() {
     ["none", "none", "none", "none"],
     ["none", "none", "Queen", "none"],
   ]);
-  
-  const [cornerColor, setCornerColor] = useState("White");
+
+  const [cornerColor, setCornerColor] = useState("white");
   const [board, setBoard] = useState([[]]);
   const [hidden, setHidden] = useState(false);
 
@@ -23,31 +23,66 @@ function Chessboard() {
   }
 
   function handleReverseRows() {
+    setCornerColor(cornerColor === "white" ? "black" : "white");
     setRows(swapRows(rows));
     setBoard(buildB());
     setMoveHistory([...moveHistory, "Reversed Rows"]);
   }
 
-  function handleHideBoard() {
-    setHidden(true);
-  }
-  function handleShowBoard() {
-    setHidden(false);
-  }
-
   function handleReverseColumns() {
+    setCornerColor(cornerColor === "white" ? "black" : "white");
     setRows(reverseColumns(rows));
     setBoard(buildB());
     setMoveHistory([...moveHistory, "Reversed Columns"]);
   }
 
-  function reverseColumns(rows){
-    setCornerColor(cornerColor == "white" ? "black" : "white");
-    return rows.map(row => row.reverse());
+  function handleAddPiece() {
+    let val = addPiece(rows, "Queen");
+    val != false ? setRows(val) : (val = "ERROR");
+    setBoard(buildB());
   }
 
-  function swapRows(rows){
-    setCornerColor(cornerColor == "white" ? "black" : "white");
+  function addPiece(rows, value) {
+    let arr = [];
+    arr = rows;
+    // Create an array to store empty indices
+    const emptyIndices = [];
+
+    // Loop through each row and column to find empty indices
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr[i].length; j++) {
+        if (arr[i][j] === "none") {
+          emptyIndices.push([i, j]); // Add empty index to array
+        }
+      }
+    }
+
+    // If there are no empty indices, return false
+    if (emptyIndices.length === 0) {
+      return false;
+    }
+
+    // Select a random empty index and set it to the given value
+    const [randRow, randCol] =
+      emptyIndices[Math.floor(Math.random() * emptyIndices.length)];
+    arr[randRow][randCol] = value;
+
+    return arr;
+  }
+
+  function handleHideBoard() {
+    setHidden(true);
+  }
+
+  function handleShowBoard() {
+    setHidden(false);
+  }
+
+  function reverseColumns(rows) {
+    return rows.map((row) => row.reverse());
+  }
+
+  function swapRows(rows) {
     return rows.reverse();
   }
 
@@ -55,7 +90,9 @@ function Chessboard() {
     const squares = [];
     for (let i = 0; i < 4; i++) {
       for (let j = 0; j < 4; j++) {
-        const squareColor = cornerColor === "white" ? ((i + j) % 2 === 0 ? "white" : "grey") : ((i + j + 1) % 2 === 0 ? "white" : "grey") ;
+        const squareColor = 
+          cornerColor === "white" ? (i + j) % 2 === 0 ? "white" : "grey"
+            : (i + j + 1) % 2 === 0 ? "grey" : "white";
         const piece = rows[j][i];
         squares.push(
           <Square
@@ -73,20 +110,41 @@ function Chessboard() {
 
   return (
     <>
-    <div className="sideBySide">
-      <div className="chessBoardContainer">
-        {hidden ? <div className="hiddenBoard"/>: <div className="chessboard">{board}</div>}
+      <div className="sideBySide">
+        <div className="chessBoardContainer">
+          {hidden ? (
+            <div className="hiddenBoard" />
+          ) : (
+            <div className="chessboard">{board}</div>
+          )}
+        </div>
+
+        <MoveHistory moveHistory={moveHistory} />
       </div>
-      
-      <MoveHistory moveHistory={moveHistory}/>
-    </div>
-    
-    <div className="bottom-container"> 
-        <button onClick={handleReverseRows} className = 'button'>Reverse Rows</button>
-        <button onClick={handleReverseColumns} className = 'button'>Reverse Columns</button>
-        {hidden ? <button onClick={handleShowBoard} className = 'button'>Show Board</button> 
-          : <button onClick={handleHideBoard} className = 'button' >Hide Board</button>}
-    <div/>
+
+      <div className="bottom-container">
+        <button onClick={handleReverseRows} className="button">
+          Reverse Rows
+        </button>
+        <button onClick={handleReverseColumns} className="button">
+          Reverse Columns
+        </button>
+        {hidden ? (
+          <button onClick={handleShowBoard} className="button">
+            Show Board
+          </button>
+        ) : (
+          <button onClick={handleHideBoard} className="button">
+            Hide Board
+          </button>
+        )}
+        <button onClick={handleAddPiece} className="button">
+          Add Piece
+        </button>
+        <button onClick={handleReverseColumns} className="button">
+          Flip Board
+        </button>
+        <div />
       </div>
     </>
   );
